3f6ecc01a825d9a66720fccc1c5b324a7a55703d
Cameron McCurdy <cam.alex.mccurdy@gmail.com>
2021-11-24 05:14:16 -0500

    Add files via upload

--- database/index.js
+++ database/index.js
@@ -2,8 +2,9 @@ const {Pool} = require('pg');
 const db = new Pool({
   user: "docker",
   password: "123456",
-  host: "db",
-  database: "product_info"
+  host: "localhost",
+  database: "product_info",
+  port: 5432
 });
 
 
